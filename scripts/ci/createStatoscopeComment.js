84dda7248b5656e305dc71495da34f0b6538ea25
melikhov <melikhov@yandex-team.ru>
2024-03-09 23:09:38 +0300

    fix

--- scripts/ci/createStatoscopeComment.js
+++ scripts/ci/createStatoscopeComment.js
@@ -2,15 +2,11 @@ const fs = require('fs');
 
 const lodashTemplate = require('lodash/template');
 
-const createPRComment = require('./createPRComment');
-
 const template = fs.readFileSync('./scripts/ci/statoscope-comment.tmpl', 'utf8');
 const compiledTemplate = lodashTemplate(template);
 
-module.exports = async ({github, context, core, downloadLink}) => {
+module.exports = ({downloadLink}) => {
     const data = JSON.parse(fs.readFileSync('result.json', 'utf8'));
     data.downloadLink = downloadLink;
-    const body = compiledTemplate(data);
-
-    await createPRComment({github, context, core, body});
+    return compiledTemplate(data);
 };
