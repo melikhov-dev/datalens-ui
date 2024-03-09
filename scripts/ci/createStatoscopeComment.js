55c0244e3b958db98c6ab11c4b29baf822c6eeea
melikhov <melikhov@yandex-team.ru>
2024-03-10 00:17:28 +0300

    wip

--- scripts/ci/createStatoscopeComment.js
+++ scripts/ci/createStatoscopeComment.js
@@ -6,7 +6,9 @@ const template = fs.readFileSync('./scripts/ci/statoscope-comment.tmpl', 'utf8')
 const compiledTemplate = lodashTemplate(template);
 
 module.exports = ({downloadLink}) => {
+    console.log(1);
     const data = JSON.parse(fs.readFileSync('result.json', 'utf8'));
+    console.log(2);
     data.downloadLink = downloadLink;
     return compiledTemplate(data);
 };
